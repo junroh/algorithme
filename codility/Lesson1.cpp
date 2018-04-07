2cec7b28677f13648d288ec55f2c490427753aba
Jun Roh <jun.roh79@gmail.com>
2018-04-07 11:09:49 +0900

    Update Lesson1.cpp
    https://app.codility.com/demo/results/trainingFBKRSX-WFK/

--- codility/Lesson1.cpp
+++ codility/Lesson1.cpp
@@ -1,16 +1,18 @@
 int solution(int N) {
-    int bin_gap = 1;
-    int cur_pos = 0;
-    int last_pos = -1;
-    while(N>0){
-        if((N&0x01)){
-            if(last_pos >= 0 && cur_pos - last_pos > bin_gap)
-                bin_gap = cur_pos - last_pos;
-            last_pos = cur_pos;
+    // write your code in C++14 (g++ 6.2.0)
+    int max_gap=0;
+    int cur_pos=0;
+    while(!(N&0x01))
+        N >>= 1;
+    while(N){
+        if(N&0x01){
+            if(max_gap<cur_pos)
+                max_gap = cur_pos;
+            cur_pos=0;    
         }
-        cur_pos++;
-        N = N >> 1;
+        else
+            ++cur_pos;
+        N >>= 1;
     }
-    bin_gap--;
-    return bin_gap;
+    return max_gap;
 }
