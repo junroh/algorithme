a30ef78ad479355864c027c44f33ff18d8169e66
Jun Roh <jroh@mimecast.com>
2020-03-13 13:50:49 +0000

    3sum

--- leetcode/3sum.java
+++ leetcode/3sum.java
@@ -1,12 +1,12 @@
 //https://leetcode.com/problems/3sum/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> noDupSet = new HashSet<>();
         if(nums==null || nums.length<3){
-            return null;
+            return new ArrayList<List<Integer>>(noDupSet);
         }
 
         int target = 0;
-        Set<List<Integer>> noDupSet = new HashSet<>();
         Arrays.sort(nums);
         for(int i=0;i< nums.length-1; i++){
             int a = nums[i];
