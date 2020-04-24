cb13c53cb6eefbc57bff7dbc02566fc76c7305c8
Jun Roh <jroh@mimecast.com>
2020-04-24 17:00:32 +0100

    https://leetcode.com/problems/bitwise-and-of-numbers-range/

---
+++ leetcode/BitwiseAndNumbersRange.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        while(m < n) 
+            n &= (n-1);
+        return n;
+    }
+};
